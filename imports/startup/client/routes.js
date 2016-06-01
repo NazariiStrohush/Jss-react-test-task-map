ff3398742255bf7aad64ae376e9fbdc5879a2dee
Nazarii <nazarii.strohush@gmail.com>
2016-06-01 17:59:19 +0300

    Added packages React-router React React-dom

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+import { Index } from '/imports/ui/components/index.jsx';
+
+Meteor.startup( () => {
+  render( 
+    <Router history={ browserHistory }>
+      <Route path="/" component={ Index } />
+    </Router>, 
+    document.getElementById( 'react-root' ) 
+  );
+});
